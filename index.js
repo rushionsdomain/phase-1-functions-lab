faa94ccf02f5811baf62a981e606d78ed0c541af
Rushion <rushionchegge@gmail.com>
2024-07-17 23:19:39 +0300

    edited JS file filfiling the tests

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solution in this file!
+// Define the headquarters block
+const hqBlock = 42;
+
+function distanceFromHqInBlocks(block) {
+  return Math.abs(block - hqBlock);
+}
+
+function distanceFromHqInFeet(block) {
+  const blocks = distanceFromHqInBlocks(block);
+  return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+  const blocksTravelled = Math.abs(start - destination);
+  return blocksTravelled * 264;
+}
+
+function calculatesFarePrice(start, destination) {
+  const distance = distanceTravelledInFeet(start, destination);
+
+  if (distance <= 400) {
+    return 0;
+  } else if (distance > 400 && distance <= 2000) {
+    return (distance - 400) * 0.02;
+  } else if (distance > 2000 && distance <= 2500) {
+    return 25;
+  } else {
+    return "cannot travel that far";
+  }
+}
